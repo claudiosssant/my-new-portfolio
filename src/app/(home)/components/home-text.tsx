5fb38273b776ecd4d8e7100f8c8f3341b105fc54
Claudio Santos <claudiogsssantos@gmail.com>
2024-04-09 23:29:01 -0300

    add about

--- src/app/(home)/components/home-text.tsx
+++ src/app/(home)/components/home-text.tsx
@@ -1,8 +1,10 @@
 import HomeBtn from "./home-cta";
+import React from 'react';
+
 
 const HomeText = () => {
   return ( 
-    <div className="flex flex-col justify-center h-full w-1/2 pl-48 gap-4">
+    <div className="flex flex-col justify-center items-center h-full w-full gap-4">
       <div className="flex">
         <h1 className="font-extrabold text-6xl">Olá, sou o Cláudio</h1><span className="font-extrabold text-6xl text-teal-300">.</span>
       </div>
@@ -10,7 +12,7 @@ const HomeText = () => {
         <span className="text-black font-bold text-2xl">Sou</span>
         <h4 className="text-teal-300 font-bold text-2xl">Desenvolvedor Full Stack</h4>
       </div>
-      <p className="text-sm w-1/2 text-left">Com foco em desenvolvimento web, e mais de um ano de experiência na área. Busco sempre expandir meu conhecimento e aprimorar minhas habilidades para continuar contribuindo de maneira significativa para meus projetos.</p>
+      <p className="text-sm w-1/5 text-center">Com foco em desenvolvimento web, e mais de um ano de experiência na área. Busco sempre expandir meu conhecimento e aprimorar minhas habilidades para continuar contribuindo de maneira significativa para meus projetos.</p>
       <HomeBtn />
     </div>
    );
