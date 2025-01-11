18a0e28e3c2e0a22cf6fa59d07594d6ebeccfa7d
Claudio Santos <claudiogsssantos@gmail.com>
2025-01-11 00:08:48 -0300

    feat: trocando foto about4

--- src/app/about/components/about-image.tsx
+++ src/app/about/components/about-image.tsx
@@ -1,12 +1,12 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
-import aboutImg from "../../../../public/craudin.jpeg"
+import aboutImg from "../../../../public/IMG_4109.jpg"
 
 const AboutImage = () => {
   return ( 
    <motion.div initial={{ scale: 0 }} animate={{ scale: 1.2 }} className="p-12">
     <div>
-      <Image width={520} height={500} src={aboutImg} alt="foto de cláudio" />
+      <Image width={320} height={300} src={aboutImg} alt="foto de cláudio" />
     </div>
    </motion.div>
    );
